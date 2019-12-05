64e5fe14358f68b18977b6c9b50e78a614439abd
Natanael <natanael_magalhaes@msn.com>
2019-12-05 19:14:37 -0300

    test callback

--- fitsbook/callbacks.py
+++ fitsbook/callbacks.py
@@ -1,5 +1,36 @@
-import requests
+from keras.callbacks import Callback
 
-def TestCallback():
-  r = requests.get('https://api.github.com/users/octocat')
-  print(r)
+import requests
+import numpy as np
+
+class TestCallback(Callback):
+  def on_train_begin(self, logs=None):
+    _logs = {}
+    for k, v in logs.items():
+      if isinstance(v, (np.ndarray, np.generic)):
+        _logs[k] = v.item()
+      else:
+        _logs[k] = v
+
+    send = {
+      'event': 'on_train_begin',
+      'model': self.model,
+      'log': _logs
+    }
+
+    requests.post('http://fitsbook.glitch.me/test', json=send)
+
+  def on_epoch_end(self, epoch, logs=None):
+    _logs = {}
+    for k, v in logs.items():
+      if isinstance(v, (np.ndarray, np.generic)):
+        _logs[k] = v.item()
+      else:
+        _logs[k] = v
+    
+    send = {
+      'event': 'on_epoch_end',
+      'log': _logs
+    }
+
+    requests.post('http://fitsbook.glitch.mr/test', json=send)
