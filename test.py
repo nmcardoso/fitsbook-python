1351675fe2e100248de1499452513513cb91ad75
Natanael <natanael_magalhaes@msn.com>
2019-12-16 01:32:07 -0300

    force a delay to training

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import numpy as np
 import os
+import time
 
 from keras import layers
 from keras import models
 from keras import optimizers
 from fitsbook.callbacks import FitsbookCallback
+from keras.callbacks import LambdaCallback
 
 def main():
   # Simple model to test API
@@ -25,7 +27,8 @@ def main():
   fahrenheit=np.array([-140,-136,-124,-112,-105,-96,-88,-75,-63,-60,-58,-40,-20,-10,0,30,35,48,55,69,81,89,95,99,105,110,120,135,145,158,160],dtype=float)
   celsius=np.array([-95.55,-93.33,-86.66,-80,-76.11,-71.11,-66.66,-59.44,-52.77,-51.11,-50,-40,-28.88,-23.33,-17.77,-1.11,1.66,8.88,12,20,27.22,31.66,35,37.22,40.55,43.33,48.88,57.22,62.77,70,71.11],dtype=float)
 
-  model.fit(fahrenheit, celsius, epochs=10, callbacks=[FitsbookCallback()])
+  lambda_cb = LambdaCallback(on_epoch_begin=lambda x, y: time.sleep(3.5))
+  model.fit(fahrenheit, celsius, epochs=10, callbacks=[lambda_cb, FitsbookCallback()])
 
 if __name__ == '__main__':
   os.environ['PY_ENV'] = 'DEV'
